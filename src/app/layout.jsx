8ad547151e027fdfc809bd07baa8a1be6c1dae19
nikhil.b <nikhil.b@darwinbox.io>
2024-04-29 17:13:05 +0530

    added basic config

---
+++ src/app/layout.jsx
@@ -0,0 +1,11 @@
+import "@styles/globals.css";
+
+export const metadata = {
+    title: "Prompt GPT",
+    description: 'Discover and Share AI Prompts'
+}
+const layout = () => {
+  return <div>layout</div>;
+};
+
+export default layout;
